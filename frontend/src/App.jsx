bc6522690f529e370344de025062835f5db0cc50
IAmGreyBunny <IAmGreyBunny@gmail.com>
2025-09-21 22:01:08 +0800

    Fixed logout in routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import NotFound from "./pages/NotFound";
 import ProtectedRoutes from "./components/ProtectedRoute";
 
 // Logs the User out by clearing all tokens
-function logout()
+function Logout()
 {
   localStorage.clear();
   return <Navigate to="/login" />
@@ -33,6 +33,7 @@ function App() {
           }
         />
         <Route path="/login" element={<Login/>}/>
+        <Route path="/logout" element={<Logout/>}/>
         <Route path="/register" element={<RegisterAndLogout/>}/>
         <Route path="*" element={<NotFound/>}/>
       </Routes>
