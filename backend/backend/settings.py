acbf8df9e734f1cf1e07ddb203b00032584bb364
IAmGreyBunny <IAmGreyBunny@gmail.com>
2025-10-16 18:21:40 +0800

    added celery setting to settings.py, created .env and it's template in file_templates

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -148,3 +148,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWS_CREDENTIALS = True
+
+# Celery Settings
+CELERY_BROKER_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
