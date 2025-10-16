9c3a12aea68cf326b8c0d5009b4ac058d4f81027
IAmGreyBunny <IAmGreyBunny@gmail.com>
2025-10-16 19:11:04 +0800

    Added working implementation of celery and also a test poll function to try out the polling

--- backend/api/tasks.py
+++ backend/api/tasks.py
@@ -1,2 +1,17 @@
 # File for setting up celery tasks
 # (e.g. carpark polling)
+from celery import shared_task
+import time
+
+
+# Test Task
+@shared_task
+def test_poll():
+    """
+    Sample task that simulates polling an external API
+    and updating your Django models.
+    """
+    print("Starting polling task...")
+    time.sleep(2)  # Simulate network delay or processing
+    print("Polling complete!")
+    return "Done"
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,7 +57,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "api",
     "rest_framework",
-    "corsheaders"
+    "corsheaders",
+    'django_celery_beat',
 ]
 
 MIDDLEWARE = [
