6ee7c471df5767a01838655834848d1c15de52c9
IAmGreyBunny <IAmGreyBunny@gmail.com>
2025-10-17 22:02:32 +0800

    removed error non-existent imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import ForgotPassword from "./pages/ForgotPassword";
-import ResetPassword from "./pages/ResetPassword";
 import Home from "./pages/Home";
 
 function App() {
