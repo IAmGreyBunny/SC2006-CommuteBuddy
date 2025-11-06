2a35857c7b1d6acd3ef64b746ee6ec9f39e4f2b3
Chandru-15 <chan.mr876@gmail.com>
2025-11-07 00:14:43 +0800

    cleaning up of codes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -6,21 +6,21 @@ import { ACCESS_TOKEN } from "./constants";
 const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 const api = axios.create({
-    baseURL: API_BASE_URL,
+    baseURL: API_BASE_URL,
 });
 
 // Interceptor to automatically add the access token of the user to requests
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem(ACCESS_TOKEN);
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
+    (config) => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
 );
 
 export default api;
